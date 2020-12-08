b058ec6e5ebb2c5163644a96ad3b8efdc8416945
Aldwinny <Aldwin0313@yahoo.com.ph>
2020-12-08 14:17:36 +0800

    Symmetry + more data

---
+++ script.js
@@ -0,0 +1,25 @@
+
+function scrollToWeb() {
+    var web = document.getElementById("web");
+    web.scrollIntoView({
+        behavior: "smooth"
+    });
+}
+
+function scrollToMob() {
+    var mob = document.getElementById("mob");
+    mob.scrollIntoView({
+        behavior: "smooth"
+    });
+}
+
+function scrollToOthers() {
+    var others = document.getElementById("others");
+    others.scrollIntoView({
+        behavior: "smooth"
+    });
+}
+
+function scrollToTop() {
+    window.scrollTo(top);
+}
