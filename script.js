5f91d17973eaf448c8a1c09d318f101fe00ba8f3
Aldwinny <aldwin0313@yahoo.com.ph>
2022-06-14 00:51:47 +0800

    Added a fixed button on mobile screens (<650px)

--- script.js
+++ script.js
@@ -5,7 +5,10 @@ let timeout;
  */
 function toggleProjects() {
   let ani = document.getElementsByClassName("projects")[0];
-  if (scrollY > 100) {
+
+  let scrollAt = ani.getBoundingClientRect().top + 100;
+
+  if (scrollY > scrollAt) {
     if (ani.classList.contains("projects-exit")) {
       if (timeout != null) {
         clearTimeout(timeout);
@@ -20,7 +23,11 @@ function toggleProjects() {
     if (ani.classList.contains("projects-animate")) {
       ani.classList.add("projects-exit");
       if (timeout == null) {
-        timeout = setTimeout(() => removeProjectsAnimation(ani), 1500);
+        if (window.innerWidth < 650) {
+          timeout = setTimeout(() => removeProjectsAnimation(ani), 500);
+        } else {
+          timeout = setTimeout(() => removeProjectsAnimation(ani), 1500);
+        }
       }
     }
   }
@@ -31,4 +38,25 @@ function removeProjectsAnimation(ani) {
   ani.classList.remove("projects-animate");
 }
 
-document.addEventListener("scroll", () => toggleProjects());
+function onWindowResize() {
+  if (window.innerWidth < 650) {
+    let act = document.querySelectorAll(".mob-area")[0];
+
+    act.classList.add("active");
+  }
+}
+
+function toggleProjectsContent() {
+  let ani = document.getElementsByClassName("projects")[0];
+  let btn = document.getElementsByClassName("proj-btn")[0];
+
+  btn.classList.toggle("web");
+  if (btn.classList.contains("web")) {
+    btn.children[0].innerText = "smartphone";
+  } else {
+    btn.children[0].innerText = "computer";
+  }
+}
+
+document.addEventListener("scroll", toggleProjects);
+document.addEventListener("resize", onWindowResize);
