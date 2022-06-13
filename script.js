22b68c4fbf7288e004d11dacb7048c4d7873c92f
Aldwinny <aldwin0313@yahoo.com.ph>
2022-06-13 22:29:39 +0800

    Basic animation entrance and exit through javascript

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
-function toggleanimate() {
-  let ani = document.getElementsByClassName("projects")[0];
+let timeout;
 
-  if (!ani.classList.contains("projects-animate")) {
-    ani.classList.add("projects-animate");
-  } else {
-    if (!ani.classList.contains("projects-exit")) {
-      ani.classList.add("projects-exit");
-    } else {
+/**
+ * Function that toggles animation classes for certain
+ */
+function toggleProjects() {
+  let ani = document.getElementsByClassName("projects")[0];
+  if (scrollY > 100) {
+    if (ani.classList.contains("projects-exit")) {
+      if (timeout != null) {
+        clearTimeout(timeout);
+        timeout = null;
+      }
       ani.classList.remove("projects-exit");
       ani.classList.remove("projects-animate");
     }
+
+    ani.classList.add("projects-animate");
+  } else {
+    if (ani.classList.contains("projects-animate")) {
+      ani.classList.add("projects-exit");
+      if (timeout == null) {
+        timeout = setTimeout(() => removeProjectsAnimation(ani), 1500);
+      }
+    }
   }
 }
+
+function removeProjectsAnimation(ani) {
+  ani.classList.remove("projects-exit");
+  ani.classList.remove("projects-animate");
+}
+
+document.addEventListener("scroll", () => toggleProjects());
